3fe5b60e7d4feecd884de8a67275a6283ade444a
PriyankaRoteSuccessive <priyanka.rote@successive.tech>
2022-03-04 18:04:03 +0530

    Done with all section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,20 @@
 
-$('.our_client').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-});
+// $('.brandlogo').slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 2000,
+// });
+// $('.stories_slider').slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 1,
+//   adaptiveHeight: true
+// });
 $('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
+  loop:true,dot:true,
+ autoplay:true,
   responsive:{
   0:{
       items:1
@@ -21,3 +27,4 @@ $('.owl-carousel').owlCarousel({
   }
   }
   })
+
